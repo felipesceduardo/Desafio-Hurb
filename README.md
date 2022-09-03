2fcbe507dc3eaec7fbfa97f617019f2b7155aa3b
Felipe Santiago Carraro Eduardo <fscarraro@gmail.com>
2022-09-03 18:56:03 -0300

    Documentação
    Documentação do código e do projeto.

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # Desafio-Hurb
- Script de um pipeline de dados desenvolvido em Python utilizando Apache Beam.
+## Descrição do Projeto
+<p align="left">Script de um pipeline de dados desenvolvido em Python utilizando Apache Beam. Este script permite ao usuário obter um arquivo csv e um json a partir de dois arquivos de dados csv. Cada um desses dois arquivos csv é passado por um pipeline de dados onde os mesmos são tratados individualmente. Ao final, os dois são agrupados e passam por um novo pipepline, sendo tratados para gerar os arquivos finais de acordo com a formatação solicitada.</p>
+ 
+## Como Rodar o Script
+<p align="left">É necessário a instalação do Python e do framework Apache Beam. O projeto foi desenvolvido utilizando a versão 3.8.6 do Python e a versão 2.41.0 do Apache Beam. Para executar o script deve-se executar "python main.py" no terminal da sua IDE ou localizar no terminal da sua máquina o path onde o projeto foi salvo e executar "python main.py" </p>
+ 
+## ETAPAS
+
+ 
+ <h3 align="left"><li><b>
+	Pipeline para tratamento dos dados do arquivo "HIST_PAINEL_COVIDBR_28set2020.csv"</li></b></h3>
+
+<p align="left">
+ A variável "covid" é uma pcollection que armazena o resultado do pipeline onde os dados da tabela 
+ "HIST_PAINEL_COVIDBR_28set2020.csv" serão carregados e tratados.
+
+ Primeiramente cada linha do arquivo csv 
+ será passado como string para a função "texto_para_lista". Essa função retornara uma lista onde cada elemento 
+ é uma substring que foi separada pelo delimitador ";". 
+ 
+ Posteriormente, a função lista_para_dicionario recebe esta lista e outra lista contendo o cabeçalho do arquivo csv e converte as duas em um dicionário onde as chaves são os elementos da lista de cabeçalho e os valores são os elementos da outra. 
+ 
+ A função "chave_coduf" recebe este dicionário, descompacta o valor da chave "coduf" e retorna uma tupla contendo este valor e o dicionário. 
+ 
+ Em seguida, o método "GroupByKey" do Apache Beam recebe esta coleção de elementos com a chave "coduf" e gera uma coleção onde cada elemento consiste de uma chave e uma lista de dicionários associados a esta chave.
+
+ A função "casos_covid" recebe esta coleção, descompacta a chave "coduf" e a lista de dicionários. Percorre através de um "for" cada dicionário e calcula o somatório do número de casos de covid e de óbitos. A saída será uma tupla contendo como primeiro elemento o código "coduf" e como segundo elemento um novo dicionário contendo UF, regiao, total de casos e total de óbitos.</p>
+
+<h3 align="left"><li><b>
+	Pipeline para tratamento dos dados do arquivo "EstadosIBGE.csv"</li></b></h3>
+
+<p align="left">
+ A variável "estados" é uma pcollection que armazena o resultado do pipeline onde os dados da tabela "EstadosIBGE.cvs" serão carregados e tratados. 
+ 
+ Primeiramente cada linha do arquivo csv será passado como string para a função "texto_para_lista". Essa função retornará uma lista onde cada elemento é uma substring que foi separada pelo delimitador ";".
+ 
+  A função lista_para_tupla
+ recebe esta lista e retorna um tupla contendo o código "coduf" e um dicionário de informações do nome do estado e do governador.
+ </p>
+
+<h3 align="left"><li><b>
+	Pipeline para tratamento da pcollection resultado"</li></b></h3>
+<p align="left">    
+ A variável "resultado" é uma pcollection que recebe as pcollections "covid" e "estados". Estas passam por um pipeline onde serão mescladas e tratadas para estarem em conformidade com o padrão do arquivo final desejado.
+ 
+  O método "Flatten" do Apache Beam é chamado para fazer o merge das duas pcollections. O resultado disso é o empilhamento das mesmas. 
+  
+  Em seguinda, o método "GroupByKey" é chamado para agrupar todos elementos que possuem em comum a chave "coduf". Este método então vai agrupar os dicionários dos elementos que possuem em comum o código "coduf" como primeiro elemento da tupla, em uma lista. O retorno será uma tupla contendo o código "coduf" e a lista do agrupamento dos dicionários. 
+  
+  Após isso, o método "Filter" do apache Beam é chamado para filtrar os elementos que deverão permanecer ou não na pcollection. Para isso foi usado a função "remove_regiao" para remover o elemento condizente a região denominada "Brasil", uma vez que este não é de interesse do projeto. 
+  
+  Posteriormente, a função "organiza_dados" é chamada para descompatar as informações de região, estado, UF, governador, total de casos de covid e de óbitos e retornar uma tupla com estes dados.
+  
+   O método "prepara_csv" é chamado para fazer a conversão da tupla em uma string onde os elementos são concatenados e delimitados por ";".
+  </p> 
+
+<h3 align="left"><li><b>
+	Geração do arquivo1 em formato csv"</li></b></h3>
+<p align="left">  
+ O resultado da pcollection anterior está pronto para gerar o arquivo1.csv. Este resultado será passado por um pipeline onde um objeto da classe "WriteToText" será instanciado passando como argumentos o nome do aquivo a ser gerado, o sufixo ".csv" e a string contendo os nomes das colunas que formarão o header do arquivo. O arquivo então é gerado quando o objeto "pipeline" é rodado.
+ </p>
+
+<h3 align="left"><li><b>
+	Geração do arquivo2 em formato json"</li></b></h3>
+<p align="left">  
+ A função "csv_para_json" é chamada passando como argumentos o arquivo csv gerado e um arquivo json vazio. Essa função sobrescreve no arquivo json onde cada coluna do arquivo csv é uma key dentro deste json.
+
+
+
--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 Este script permite ao usuário obter um arquivo csv e um json a partir
 de dois arquivos de dados csv.
 Cada um desses dois arquivos csv é passado por um pipeline de dados 
-onde os mesmos são tratados individualmente. Ao final os dois são agrupados 
+onde os mesmos são tratados individualmente. Ao final, os dois são agrupados 
 e passam por um novo pipepline, sendo tratados para gerar os arquivos finais
 de acordo com a formatação solicitada.
 
-Este script requer instalação do framework Apache Beam
+O script requer instalação do framework Apache Beam
 """
 import csv 
 import json 
@@ -95,10 +95,19 @@ def chave_coduf(elemento):
     return (coduf, elemento)
 
 def casos_covid(elemento):
-    """
-    Recebe uma tupla (coduf, [{}, {}, {}...])
-    Retorna uma tupla contendo o coduf e um dicionário contendo regiao, estado, coduf, totalCasos, totalObitos
-    """
+    """Recebe uma tupla e retorna uma tupla contendo o códido da UF e um dicionário
+
+    Parametros
+    ----------
+    elemento : tupla
+        Tupla contendo código "coduf" e uma lista de dicionários onde tem em comum este código 
+
+    Retorna
+    -------
+    Tupla
+        Tupla contendo o código "coduf" e um dicionário contendo informações da região, estado, código da UF, total 
+        de casos e total de óbitos
+    """    
     coduf, registros = elemento
     totalCasos = 0
     totalObitos = 0
@@ -118,10 +127,18 @@ def casos_covid(elemento):
     return (coduf, dicionario)
 
 def lista_para_tupla(elemento):
-    """
-    Recebe lista 
-    Retorna tupla com coduf e um dicionário contendo nome do estado e do governador
-    """
+    """Recebe uma lista e retorna uma tupla contendo o códido da UF e um dicionário
+
+    Parametros
+    ----------
+    elemento : lista
+        Lista onde cada elemento corresponde a uma célula de uma linha do arquivo "EstadosIBGE.csv" 
+
+    Retorna
+    -------
+    Tupla
+        Tupla contendo o código "coduf" e um dicionário contendo informações do nome do estado e do governador
+    """    
     nome_estado = elemento[0]
     coduf = elemento[1]
     governador = elemento[3]
@@ -132,20 +149,38 @@ def lista_para_tupla(elemento):
     return (coduf, dicionario)
 
 def remove_regiao(elemento):
-    """
-    Recebe tupla 
-    Remove a região denominada Brasil pelo código UF
-    """
+    """Recebe uma tupla e a retorna caso satisfa a condicional 
+
+    Parametros
+    ----------
+    elemento : tupla
+        tupla contendo código "coduf" e uma lista de dicionários
+
+    Retorna
+    -------
+    Tupla
+        tupla contendo código "coduf" e uma lista de dicionários
+    """     
+    
     coduf, lista_dicionarios = elemento
-    if(coduf == '76'):
-        return False  
-    return True
+    if(coduf != '76'):
+        return True  
+    return False
 
 def organiza_dados(elemento):
-    """
-    Recebe uma tupla 
-    Retorna uma tupla com colunas organizadas conforme solicitado no projeto
-    """
+    """Recebe uma tupla e a retorna com elementos organizadas conforme desejado pelo projeto
+
+    Parametros
+    ----------
+    elemento : tupla
+        tupla contendo código "coduf" e uma lista de dicionários
+
+    Retorna
+    -------
+    Tupla
+        tupla contendo informações do nome da região, estado, governador, total de casos e de óbitos
+    """  
+
     coduf, lista_dicionarios = elemento
     dic1 = lista_dicionarios[0]
     dic2 = lista_dicionarios[1]
@@ -158,33 +193,47 @@ def organiza_dados(elemento):
     return (regiao, estado, uf, governador, str(totalCasos), str(totalObitos))
 
 def prepara_csv(elemento, delimitador=";"):
+    """Recebe uma tupla e retorna uma string com os elementos delimitados por ";"
+
+    Parametros
+    ----------
+    elemento : tupla
+        tupla contendo os elementos desejados pelo projeto
+
+    Retorna
+    -------
+    String
+        string formada pela concatenação de cada elemento da tupla delimitados pelo caractere ';'
     """
-    Receber uma tupla do tipo 
-    Retornar uma string delimitada por ';'
-    """
+    
     return f"{delimitador}".join(elemento)    
 
 
-def csv_para_json(csvArquivo, jsonArquivo):
+def csv_para_json(arquivo_csv, arquivo_json):
+    """Recebe um arquivo csv e um json vazio e escreve neste último o conteúdo do arquivo csv
+
+    Parametros
+    ----------
+    arquivo_csv : csv
+        arquivo csv contendo o conteúdo do arquivo1 gerado pelo pipeline
+
+    Retorna
+    -------
+    json
+        arquivo json 
     """
-    Recebe dois arquivos, um csv e outro json
-    Lê o arquivo csv e carrega seus dados usando o leitor de dicionário da biblioteca csv
-    Converte cada linha do csv em um dicionário
-    Adiciona o dicionário em um array 
-    Converte o array em uma string JSON e escreve no arquivo
-    """
+
     json_array = [] 
-    with open(csvArquivo, encoding='utf-8') as csvf: 
+    with open(arquivo_csv, encoding='utf-8') as csvf: 
         reader = csv.DictReader(csvf, delimiter=';')  
         for row in reader: 
             json_array.append(row)
 
-    with open(jsonArquivo, 'w', encoding='utf-8') as jsonf: 
-        jsonString = json.dumps(json_array, indent=4)
-        jsonf.write(jsonString)
+    with open(arquivo_json, 'w', encoding='utf-8') as jsonf: 
+        json_string = json.dumps(json_array, indent=4)
+        jsonf.write(json_string)
     
 
-#pcollection gerado a partir do pipeline que trata os dados do arquivo HIST_PAINEL_COVIDBR_28set2020.csv
 covid = (
     pipeline
     | "Leitura do dataset de casos de covid" >> 
@@ -194,10 +243,10 @@ covid = (
     | "Criar chave pelo código da UF" >> beam.Map(chave_coduf)
     | "Agrupar pelo estado" >> beam.GroupByKey()
     | "Descompactar casos de covid" >> beam.Map(casos_covid)
-   # | "Mostrar resultados" >> beam.Map(print)   
+    #| "Mostrar resultados" >> beam.Map(print)   
 )
 
-#pcollection gerado a partir do pipeline que trata os dados do arquivo EstadosIBGE.csv
+
 estados = (
     pipeline
     | "Leitura do dataset de informações dos estados" >> 
